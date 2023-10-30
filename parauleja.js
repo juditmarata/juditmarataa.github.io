7f63d52dcc4b26d77685fed266f20e9ccefd4fe4
juditmarata <145449465+juditmarata@users.noreply.github.com>
2023-10-30 08:25:16 +0100

    Add files via upload

---
+++ parauleja.js
@@ -0,0 +1,800 @@
+animals = [
+    'Àguila',
+    'Ànec',
+    'Aligot',
+    'Alpaca',
+    'Antílop',
+    'Armadillo',
+    'Albatros',
+    'Aranya',
+    'Abellot',
+    'Abelles',
+    'Abellerol',
+    'Balena',
+    'Búfal',
+    'Boví',
+    'Bisó',
+    'Babuí',
+    'Beluga',
+    'Burru',
+    'Barracuda',
+    'Bulldog',
+    'Borinot',
+    'Cavall',
+    'Canari',
+    'Cacatua',
+    'Cabra',
+    'Cangur',
+    'Camell',
+    'Cucut',
+    'Cocodril',
+    'Colibrí',
+    'Cèrvol',
+    'Corb',
+    'Cargol',
+    'Colom',
+    'Carpa',
+    'Cuc',
+    'Conill',
+    'Dofí',
+    'Dromedari',
+    'Daina',
+    'Dragó',
+    'Elefant',
+    'Esquirol',
+    'Escamarlà',
+    'Estornell',
+    'Escarabat',
+    'Escurçó',
+    'Falcó',
+    'Fènix',
+    'Foca',
+    'Flamenc',
+    'Formiga',
+    'Falciot',
+    'Furó',
+    'Faisà',
+    'Fura',
+    'Gat',
+    'Gos',
+    'Gallina',
+    'Gavina',
+    'Goril·la',
+    'Gall',
+    'Gamba',
+    'Guepard',
+    'Granota',
+    'Guineu',
+    'Guatlla',
+    'Gavatx',
+    'Girafa',
+    'Hiena',
+    'Hipopòtam',
+    'Hàmster',
+    'Hortolà',
+    'Iguana',
+    'Impala',
+    'Indri',
+    'Isard',
+    'Igüana',
+    'Jaguar',
+    'Jerbo',
+    'Koala',
+    'Kiwi',
+    'Krill',
+    'Lleó',
+    'Llop',
+    'Llúdriga',
+    'Llagosta',
+    'Liró',
+    'Lluç',
+    'Mico',
+    'Marmota',
+    'Mandril',
+    'Mosca',
+    'Mosquit',
+    'Morsa',
+    'Musaranya',
+    'Mussol',
+    'Manatí',
+    'Nandú',
+    'Naso',
+    'Orangutan',
+    'Oriol',
+    'Ocell',
+    'Os',
+    'Orca',
+    'Puma',
+    'Panda',
+    'Pitó',
+    'Poni',
+    'Perdiu',
+    'Papallona',
+    'Pardal',
+    'Quetzal',
+    'Rinoceront',
+    'Rata',
+    'Rossinyol',
+    'Ratolí',
+    'Senglar',
+    'Serp',
+    'Sargantana',
+    'Sardina',
+    'Salamandra',
+    'Sarg',
+    'Saltamartí',
+    'Tigre',
+    'Tortuga',
+    'Tonyina',
+    'Tauró',
+    'Taràntula',
+    'Teixó',
+    'Tòtil',
+    'Ualabí',
+    'Vaca',
+    'Vedella',
+    'Vespa',
+    'Vibora',
+    'Wapití',
+    'Ximpanzé',
+    'Xinxilla',
+    'Nyu',
+    'Zebra'
+    ]
+
+plantes = [
+  'All',
+  'Aloe',
+  'Ametller',
+  'Arboç',
+  'Anemone',
+  'Anturium',
+  'Angelònia',
+  'Alstremèria',
+  'Begònia',
+  'Bambú',
+  'Belladona',
+  'Bromèlia',
+  'Clavellina',
+  'Clavell',
+  'Cactus',
+  'Cannàbis',
+  'Cirerer',
+  'Camèlia',
+  'Dàlia',
+  'Dent de lleó',
+  'Dipladenia',
+  'Eufòrbia',
+  'Espígol',
+  'Eucaliptus',
+  'Ferigola',
+  'Fong',
+  'Fúcsia',
+  'Fesolera',
+  'Ginesta',
+  'Girasol',
+  'Gerani',
+  'Grèvol',
+  'Hortència',
+  'Heura',
+  'Iris',
+  'Iuca peu elefant',
+  'Jonc',
+  'Juniperus',
+  'Kalanchoe',
+  'Lliri',
+  'Lilium',
+  'Margarida',
+  'Mimosa',
+  'Muguet',
+  'Magnòlia',
+  'Malva',
+  'Menta',
+  'Narcís',
+  'Nenúfar',
+  'Noguera',
+  'Orquídea',
+  'Olivera',
+  'Orenga',
+  'Onagra',
+  'Pensament',
+  'Pomer',
+  'Presseguer',
+  'Palmera',
+  'Pavònia',
+  'Quercus',
+  'Rosa',
+  'Roure',
+  'Romaní',
+  'Rododendre',
+  'Solitària',
+  'Sàlvia',
+  'Sempreviva',
+  'Siclàmen',
+  'Suculenta',
+  'Tulipa',
+  'Taronger',
+  'Trèvol',
+  'Til·ler',
+  'Ulmaria',
+  'Umbaúba',
+  'Violeta',
+  'Vinya',
+  'Verònica',
+  'Verato',
+  'Watsonia',
+  'Wadelia',
+  'Xanthorrhoea',
+  'Xanana',
+  'Yucca',
+  'Zamioculca',
+]
+
+professions = [
+  'Advocat',
+  'Advocada',
+  'Arquitecte',
+  'Arquitecta',
+  'Agent de viatges',
+  'Agricultor',
+  'Agricultora',
+  'Astrònom',
+  'Astrònoma',
+  'Actor',
+  'Actriu',
+  'Arqueòleg',
+  'Arqueòloga',
+  'Agent de seguretat',
+  'Astronauta',
+  'Assistenta social',
+  'Assistent social',
+  'Bibliotecari',
+  'Bibliotecària',
+  'Biòleg',
+  'Biològa',
+  'Bomber',
+  'Bombera',
+  'Barber',
+  'Barbera',
+  'Banquer',
+  'Banquera',
+  'Botànic',
+  'Botànica',
+  'Barista',
+  'Botiguer',
+  'Botiguera',
+  'Boticari',
+  'Boticària',
+  'Cuiner',
+  'Cuinera',
+  'Cambrer',
+  'Cambrera',
+  'Científic',
+  'Científica',
+  'Carpinter',
+  'Carpintera',
+  'Comissari',
+  'Comissària',
+  'Cantant',
+  'Capellà',
+  'Dentista',
+  'Doctor',
+  'Doctora',
+  'Enginyer',
+  'Enginyera',
+  'Electricista',
+  'Esportista',
+  'Economista',
+  'Fisioterapeuta',
+  'Farmacèutic',
+  'Forner',
+  'Fornera',
+  'Farmacèutica',
+  'Fotògraf',
+  'Fotògrafa',
+  'Geòleg',
+  'Geòloga',
+  'Ginecòleg',
+  'Ginecòloga',
+  'Gestor',
+  'Gestora',
+  'Historiador',
+  'Historiadora',
+  'Infermer',
+  'Infermera',
+  'Informàtic',
+  'Informàtica',
+  'Jutge',
+  'Jutgessa',
+  'Joier',
+  'Músic',
+  'Metge',
+  'Metgessa',
+  'Mecànic',
+  'Mecànica',
+  'Nutricionista',
+  'Novel·lista',
+  'Notari',
+  'Notaria',
+  'Operador',
+  'Operadora',
+  'Oftalmòleg',
+  'Oftalmòloga',
+  'Policia',
+  'Pilot',
+  'Psicòleg',
+  'Psicòloga',
+  'Professor',
+  'Professora',
+  'Químic',
+  'Química',
+  'Revisor',
+  'Revisora',
+  'Robotista',
+  'Sociòleg',
+  'Sociòloga',
+  'Soldat',
+  'Socorrista',
+  'Stremer',
+  'Traductor',
+  'Traductora',
+  'Veterinari',
+  'Veterinària',
+  'Venedor',
+  'Venedora',
+  'Xilògraf',
+  'Xilògrafa',
+  'Youtuber',
+  'Youtuber',
+  'Zoòleg',
+  'Zoòloga'
+]
+
+nom = [
+    'Adrià',
+    'Alba',
+    'Aleix',
+    'Àlex',
+    'Anna',
+    'Arnau',
+    'Aitana',
+    'Arlet',
+    'Ariel',
+    'Albert',
+    'Arnald',
+    'Bernat',
+    'Berta',
+    'Bru',
+    'Bruna',
+    'Bruno',
+    'Blai',
+    'Carla',
+    'Cristina',
+    'Carles',
+    'Carolina',
+    'Carme',
+    'Clara',
+    'Cèsar',
+    'David',
+    'Diana',
+    'Dídac',
+    'Dolors',
+    'Dunia',
+    'Damià',
+    'Dan',
+    'Daniel',
+    'Elena',
+    'Eduard',
+    'Enric',
+    'Estel',
+    'Esther',
+    'Ester',
+    'Eloi',
+    'Emma',
+    'Eric',
+    'Eva',
+    'Ferran',
+    'Farners',
+    'Fàtima',
+    'Francesc',
+    'Gerard',
+    'Guillem',
+    'Gael',
+    'Georgina',
+    'Gisela',
+    'Glòria',
+    'Gilbert',
+    'Hugo',
+    'Helena',
+    'Haitam',
+    'Hatim',
+    'Isabel',
+    'Isona',
+    'Irene',
+    'Inés',
+    'Iolanda',
+    'Ignasi',
+    'Ingrid',
+    'Iria',
+    'Ismael',
+    'Iu',
+    'Ivan',
+    'Ivet',
+    'Jan',
+    'Jana',
+    'Joan',
+    'Jaume',
+    'Javier',
+    'Javi',
+    'Judit',
+    'Joana',
+    'Joel',
+    'Jofre',
+    'Jon',
+    'Josep',
+    'Judith',
+    'Júlia',
+    'Jordi',
+    'Júlia',
+    'Kai',
+    'Karim',
+    'Karina',
+    'Karla',
+    'Kevin',
+    'Laura',
+    'Leire',
+    'Lídia',
+    'Lluís',
+    'Lola',
+    'Lucas',
+    'Laia',
+    'Lluc',
+    'Lucia',
+    'Marc',
+    'Maria',
+    'Magda',
+    'Manel',
+    'Mar',
+    'Marçal',
+    'Marcel',
+    'Mariam',
+    'Maribel',
+    'Marina',
+    'Mario',
+    'Mariona',
+    'Martina',
+    'Matias',
+    'Matia',
+    'Murici',
+    'Maure',
+    'Max',
+    'Meritxell',
+    'Miquel',
+    'Mohamed',
+    'Mònica',
+    'Montse',
+    'Marta',
+    'Martí',
+    'Mireia',
+    'Nil',
+    'Nora',
+    'Nadin',
+    'Neus',
+    'Noelia',
+    'Núria',
+    'Oliver',
+    'Ona',
+    'Oriol',
+    'Olga',
+    'Òscar',
+    'Pau',
+    'Pol',
+    'Paula',
+    'Pedro',
+    'Pep',
+    'Pere',
+    'Quim',
+    'Queralt',
+    'Quico',
+    'Roger',
+    'Rafel',
+    'Raimon',
+    'Ramon',
+    'Raquel',
+    'Raül',
+    'Raúl',
+    'Ricard',
+    'Rita',
+    'Roc',
+    'Ruth',
+    'Rayan',
+    'Rut',
+    'Sara',
+    'Sergi',
+    'Sofia',
+    'Sandra',
+    'Selena',
+    'Serafin',
+    'Sergi',
+    'Sílvia',
+    'Sofian',
+    'Tània',
+    'Tomàs',
+    'Toni',
+    'Txell',
+    'Úrsula',
+    'Unai',
+    'Víctor',
+    'Valèria',
+    'Varuni',
+    'Víctor',
+    'Xavi',
+    'Xavier',
+    'Xènia',
+    'Yasmin',
+    'Yvonne',    
+    'Yeray',
+    'Zoe'
+]
+
+menjar = [
+    'Arros', 
+    'Alvocat', 
+    'Api', 
+    'Amanida',
+    'Bacallà', 
+    'Bistec', 
+    'Brou',
+    'Carn',
+    'Ceba',
+    'Cargols',
+    'Croissant', 
+    'Clementina',
+    'Dàtils', 
+    'Dorada', 
+    'Donut', 
+    'Datilera',
+    'Embotit', 
+    'Enciam', 
+    'Esparrec', 
+    'Estofat',
+    'Fruita',
+    'Formatge',
+    'Figues',
+    'Faves',
+    'Garrofes', 
+    'Gelat', 
+    'Galetes',
+    'Hamburguesa', 
+    'Hummus', 
+    'Iogurt', 
+    'Julivert', 
+    'Jalapeño', 
+    'Jamón',
+    'Kiwi', 
+    'Ketchup', 
+    'Kebab', 
+    'Kaki',
+    'Llet', 
+    'Llimona', 
+    'Llenties', 
+    'Llengua',
+    'Mandarina', 
+    'Meló', 
+    'Macarrons', 
+    'Mandonguilles',
+    'Noodles', 
+    'Nabius', 
+    'Nectarina',
+    'Oli',
+    'Orenga', 
+    'Ou', 
+    'Orada',
+    'Oliva',
+    'Pasta', 
+    'Poma',
+    'Plàtan', 
+    'Pilota', 
+    'Pinya',
+    'Quesadilla', 
+    'Quinua', 
+    'Risotto', 
+    'Rovelló',
+    'Ratatouille',
+    'Salmó', 
+    'Síndria',
+    'Sardina', 
+    'Taronja', 
+    'Tòfona', 
+    'Tomàquet', 
+    'Ulleres',
+    'Vermut',
+    'Vainilla', 
+    'Xocolata', 
+    'Xai', 
+    'Xurros',
+    'Yakisoba',
+    'Za atar', 
+    'Zopf'
+]
+
+image_array = [
+    '1.png',
+    '2.png',
+    '3.png',
+    '5.png',
+    '6.png'
+]
+
+combinacio_array = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+
+arraysTematica = []
+let comptadorId;
+let comptadorNumero = document.querySelector('.comptador'); 
+let tematica = document.getElementById('tematica');
+
+
+function get_random_imagecolor(){
+
+    random_index = Math.floor(Math.random() * image_array.length);
+    random_index3 = Math.floor(Math.random() * combinacio_array.length);
+    selected_image = image_array[random_index]
+    selected_combinacio = combinacio_array[random_index3]
+    document.getElementById('random_img').src = `./img/${selected_image}`
+    document.getElementById('lletres').textContent = selected_combinacio
+    
+    comptadorNumero.style.display = 'block';
+    document.querySelector(".respostaIA").style.display = "none";
+
+    comptador(selected_image);
+    reiniciar(selected_image);
+  }
+
+
+function comptador(selected_image){
+  let comptador = 10;
+
+  comptadorId = setInterval(()=>{
+  comptador--;
+
+  if(comptador === 0){
+    clearInterval(comptadorId)
+    respostaIA(selected_image, selected_combinacio);
+  }else{
+  comptadorNumero.innerText = comptador;
+  }
+},1000)
+}
+
+function guardar(){
+  var resposta = document.getElementById("resposta").value;
+  comprovar(resposta);
+}
+
+function RespostaCorrecta(resposta, arrays){
+  for (var i = 0; i < arrays.length; i++) {
+    if(arrays[i].includes(resposta)){
+      return true;
+    }
+  }
+  return false;
+}
+
+function comprovar(resposta){
+  arraysTematica = [animals, menjar, professions, nom, plantes];
+  
+  if (RespostaCorrecta(resposta, arraysTematica)){
+    let Correcta = 'Correcta';
+    document.querySelector(".solucio").style.display = "block";
+    document.querySelector(".solucio .text").innerText = Correcta;
+  }
+  else{ 
+    let Incorrecta = 'Incorrecta';
+    document.querySelector(".solucio").style.display = "block";
+    document.querySelector(".solucio .text").innerText = Incorrecta;
+
+    respostaIA(selected_image, selected_combinacio)
+
+  }
+}
+
+function respostaIA(selected_image, selected_combinacio) {
+
+  if (selected_image === '1.png') {
+      for (var i = 0; i < combinacio_array.length; i++){
+        if(combinacio_array[i] === selected_combinacio){
+          var lletra = selected_combinacio;
+          break;
+        }
+      }
+      if(lletra){
+        for(var j = 0; j < animals.length; j++){
+          if(animals[j].startsWith(lletra)){
+            document.querySelector(".respostaIA").style.display = "block";
+            document.querySelector(".respostaIA .text").innerText = animals[j];
+            break;
+          }
+        }
+      }
+  }
+  else if (selected_image === '2.png'){
+    for (var i = 0; i < combinacio_array.length; i++){
+      if(combinacio_array[i] === selected_combinacio){
+        var lletra = selected_combinacio;
+        break;
+      }
+    }
+    if(lletra){
+      for(var j = 0; j < professions.length; j++){
+        if(professions[j].startsWith(lletra)){
+          document.querySelector(".respostaIA").style.display = "block";
+          document.querySelector(".respostaIA .text").innerText = professions[j];
+          break;
+        }
+      }
+    }
+  }
+  else if (selected_image === '3.png'){
+    for (var i = 0; i < combinacio_array.length; i++){
+      if(combinacio_array[i] === selected_combinacio){
+        var lletra = selected_combinacio;
+        break;
+      }
+    }
+    if(lletra){
+      for(var j = 0; j < plantes.length; j++){
+        if(plantes[j].startsWith(lletra)){
+          document.querySelector(".respostaIA").style.display = "block";
+          document.querySelector(".respostaIA .text").innerText = plantes[j];  
+        break;
+        }
+      }
+    }
+}
+  else if (selected_image === '5.png'){
+    for (var i = 0; i < combinacio_array.length; i++){
+      if(combinacio_array[i] === selected_combinacio){
+        var lletra = selected_combinacio;
+        break;
+      }
+    }
+    if(lletra){
+      for(var j = 0; j < nom.length; j++){
+        if(nom[j].startsWith(lletra)){
+          document.querySelector(".respostaIA").style.display = "block";
+            document.querySelector(".respostaIA .text").innerText = nom[j];
+          break;
+        }
+      }
+    }
+}
+  else if (selected_image === '6.png'){
+    for (var i = 0; i < combinacio_array.length; i++){
+      if(combinacio_array[i] === selected_combinacio){
+        var lletra = selected_combinacio;
+        break;
+      }
+    }
+    if(lletra){
+      for(var j = 0; j < menjar.length; j++){
+        if(menjar[j].startsWith(lletra)){
+          document.querySelector(".respostaIA").style.display = "block";
+            document.querySelector(".respostaIA .text").innerText = menjar[j];
+          break;
+        }
+      }
+    }
+}
+}
+
+
+function reiniciar(selected_image){
+  clearInterval(comptadorId);
+  comptador(selected_image);
+}
+
+get_random_imagecolor();
+
